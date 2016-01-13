c6528432c507dbce9c7f4a2396302c27b32739bc
Justin Firth <jmfirth@gmail.com>
2016-01-12 23:13:01 -0500

    Component generator cleanup

--- generators/component/index.js
+++ generators/component/index.js
@@ -14,12 +14,9 @@ module.exports = yeoman.generators.Base.extend({
     return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
   }).replace(/\s+/g, '');
     this.ProperName = this.name.replace(/\w*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1)})
-  },
-  prompting: function () {
-
+    this.hyphenName = this.camelName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
   },
   writing: function () {
-
     this.template('_component.elm', 'src/elm/spa/App/Components/' + this.ProperName + '.elm')
   }
 });
